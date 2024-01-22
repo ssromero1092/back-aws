006cfc53b3a8e0c4647327f4b612c0622f929843
ssromero <ssromero@lhdatagroup.cl>
2024-01-22 10:10:09 -0500

    Actualizacion

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
-// backend/src/server.ts
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { getTodos, createTodo, updateTodo, deleteTodo } from './controllers/todoController';
+import * as httpToHttps from 'express-http-to-https';
 
 const app = express();
 const port = 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+// Middleware para redirigir HTTP a HTTPS
+app.use(httpToHttps.redirectToHTTPS());
 
 // Rutas para CRUD de todos
 app.get('/todos', getTodos);
@@ -17,5 +20,5 @@ app.put('/todos/:id', updateTodo);
 app.delete('/todos/:id', deleteTodo);
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en  http://localhost:${port}`);
-});
+  console.log(`Servidor corriendo en https://localhost:${port}`);
+}); 
