40115153d1cab2771d275bb4499acc34d1f5e5d6
ssromero <ssromero@lhdatagroup.cl>
2024-01-24 05:14:58 -0500

    Actualizacion

---
+++ src/core/connection.ts
@@ -0,0 +1,11 @@
+import { createClient } from "redis";
+
+export const connect =async () => {
+    const client =createClient();
+    client.on("error", err=> console.log("Redis client error", err));
+    console.log("Success");
+    await client.connect();
+    return client 
+
+    
+}
